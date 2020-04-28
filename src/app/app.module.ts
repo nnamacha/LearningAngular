7a939c18e930de0de18a48544a54d3442726850c
nb181916 <NicholasN@Nedbank.co.za>
2020-04-28 07:48:49 +0200

    After adding outsourced routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,21 +12,9 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const appRoutes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent , children: [
-    { path: ':id/:name', component: UserComponent }
-  ]},
 
-  { path: 'servers', component: ServersComponent , children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent }
-  ]},
-  { path: 'not-found', component: NotFoundComponent },
-  { path: '**' , redirectTo: '/not-found'}
-
-];
 
 @NgModule({
   declarations: [
@@ -42,7 +30,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes),
+    AppRoutingModule
+
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
